66f3b327f3b0d5b404d5987249a7805ad010da96
Pavel Vasilev <karterpahan@gmail.com>
2023-08-31 17:05:34 +0300

    create UI

---
+++ src/components/UI-components/CarouselImg.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { Carousel } from 'react-bootstrap'
+import img from "./../../img/veb-razrabotchik-918x516.jpg"
+import ImgWindow from './ImgWindow';
+
+const CarouselImg = () => {
+ 
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex) => {
+        setIndex(selectedIndex);
+    };
+
+  return (
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+          <Carousel.Item>
+              <img
+                //   className="d-block w-100"
+                  className="img-fluid"
+                  src={img}
+                  alt="First slide"
+              />
+              {/* <ImgWindow text="First slide" /> */}
+              <Carousel.Caption>
+                  <h3>First slide label</h3>
+                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+              <img
+                  className="img-fluid"
+                  src={img}
+                  alt="First slide"
+              />
+              {/* <ImgWindow text="Second slide" /> */}
+              <Carousel.Caption>
+                  <h3>Second slide label</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+              <img
+                  className="img-fluid"
+                  src={img}
+                  alt="First slide"
+              />
+              {/* <ImgWindow text="Third slide" /> */}
+              <Carousel.Caption>
+                  <h3>Third slide label</h3>
+                  <p>
+                      Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                  </p>
+              </Carousel.Caption>
+          </Carousel.Item>
+      </Carousel>
+  )
+}
+
+export default CarouselImg
---
+++ src/components/UI-components/OffcanvasEnd.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { Button, Offcanvas } from 'react-bootstrap'
+
+const OffcanvasEnd = ({ placement }) => {
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
+  return (
+      <>
+          <Button variant="secondary" onClick={handleShow} className="ms-2">
+             more about me
+          </Button>
+          <Offcanvas show={show} onHide={handleClose} placement={placement}>
+              <Offcanvas.Header closeButton>
+                  <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                  Some text as placeholder. In real life you can have the elements you
+                  have chosen. Like, text, images, lists, etc.
+              </Offcanvas.Body>
+          </Offcanvas>
+      </>
+  )
+}
+
+export default OffcanvasEnd
